3f0e77f00ec7c5519767ccefd11eb5b902c55697
张志强 <58879657+zhangzhiqiangccm@users.noreply.github.com>
2020-08-27 07:03:10 +0800

    各种排序

---
+++ various_sort/bubble_sort.py
@@ -0,0 +1,21 @@
+
+
+import random
+from cal_time import *
+
+@cal_time
+def bubble_sort(li):
+    for i in range(len(li)-1):  #第i趟
+        exchange = False
+        for j in range(len(li)-i-1):
+            if li[j] > li[j+1]:
+                li[j], li[j+1] = li[j+1], li[j]
+                exchange = True
+        if not exchange:
+            return
+
+
+li = list(range(10000))
+random.shuffle(li)
+
+bubble_sort(li)
---
+++ various_sort/count_sort.py
@@ -0,0 +1,26 @@
+
+
+from cal_time import *
+
+@cal_time
+def count_sort(li, max_count=100):
+    count = [0 for _ in range(max_count+1)]
+    for val in li:
+        count[val] += 1
+    li.clear()
+    for ind, val in enumerate(count):
+        for i in range(val):
+            li.append(ind)
+
+@cal_time
+def sys_sort(li):
+    li.sort()
+
+import random, copy
+li = [random.randint(0,100) for _ in range(100000)]
+
+li1 = copy.deepcopy(li)
+li2 = copy.deepcopy(li)
+
+count_sort(li1)
+sys_sort(li2)
