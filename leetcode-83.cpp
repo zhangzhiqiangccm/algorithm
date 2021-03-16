be706cbf6eab70e90e7df1d2e5698761c2ed49c9
张大喵 <58879657+zhangzhiqiangccm@users.noreply.github.com>
2021-03-16 21:30:38 +0800

    leetcode83

---
+++ leetcode-83.cpp
@@ -0,0 +1,30 @@
+#include "common.h"
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
+class Solution {
+ public:
+  ListNode *deleteDuplicates(ListNode *head) {
+    if (head == nullptr) return nullptr;
+
+    ListNode *p = head;
+    while (p->next) {
+      if (p->val == p->next->val) {
+        p->next = p->next->next;
+      } else {
+        p = p->next;
+      }
+    }
+
+    return head;
+  }
+};
